7c45a11014d433fc87e0b35acc8dd4f7c44fe55d
Xhandler <chandlerd@gmail.com>
2018-03-09 11:10:44 -0500

    formatting for deployment

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -3,13 +3,14 @@ var path = require('path');
 module.exports = function(app){
     // get request handler for root - serves up index.html from the public directory
     app.get('/', function(req, res){
-        res.sendFile(path.join(__dirname, '/../public/golf.html'))
+        res.sendFile(path.join(__dirname, '../index.html'))
+        // res.sendFile(path.join(__dirname, './style.css'))
     });
     // get request handler for /game - serves up index.html from the public directory
-    app.get('/game', function(req, res){
+    app.get('/golf', function(req, res){
         res.sendFile(path.join(__dirname, '/../public/golftest.html'))
     })
     app.get('/index', function(req, res){
-        res.sendFile(path.join(__dirname, '/../public/index.html'))
+        res.sendFile(path.join(__dirname, '/index.html'))
     })
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Matter = require('matter-js');
-
+var path= require('path');
+var fs = require('fs');
 var app = express();
 
 var PORT = process.env.PORT || 3001;
@@ -10,12 +11,12 @@ var PORT = process.env.PORT || 3001;
 //
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('./node_modules'));
+app.use(express.static(path.join(__dirname,'public')));
 // requiring function and passing app through as an argumentter
 require('./routing/apiRoutes.js')(app);
 require('./routing/htmlRoutes.js')(app);
 
 
 app.listen(PORT, function(){
-    console.log('App is lstening on port: ' + PORT)
+    console.log('App is lstening on port: ' + PORT)  // res.sendFile(path.join(__dirname, './style.css'))
 })
