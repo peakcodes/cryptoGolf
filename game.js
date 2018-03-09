f2685c4c5c5874bc10bf16bb3f8343b94e3c1aa9
Xhandler <chandlerd@gmail.com>
2018-03-08 23:35:16 -0500

    Cleaned up code, adjusted render height, and forced ES6 on the group :)

---
+++ game.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const golf = async () => {
+
+	// module aliases
+	const Engine = Matter.Engine,
+		Render = Matter.Render,
+		World = Matter.World,
+		Constraint = Matter.Constraint,
+		Mouse = Matter.Mouse,
+		MouseConstraint = Matter.MouseConstraint,
+		Composite = Matter.Composite,
+		Composites = Matter.Composites,
+		Body = Matter.Body,
+		Bodies = Matter.Bodies,
+		Vector = Matter.Vector,
+		Vertices = Matter.Vertices;
+
+	// create an engine
+	const engine = Engine.create();
+	// create a renderer
+	const render = Render.create({
+		element: document.body,
+		engine: engine
+	});
+
+	const coins = ["BTC/USD", "LTC/USD", "ETH/USD", "XMR/USD", "LTC/BTC", "ETH/BTC", "XMR/BTC"];
+	const tFrame = ["1m", "1h", "1D"];
+	let points = [];
+
+	const pullCandleClose = async () => {
+
+		let queryURL = "https://api.bitfinex.com/v2/candles/trade:1m:tBTCUSD/hist";
+		let coin = '';
+		let response = await fetch(queryURL);
+		// console.log(response);
+		let graphData = await response.json();
+		// console.log(htmlString);      var points = [];
+
+		for (let i = 0; i < 30; i++) {
+			points.push(Math.floor(graphData[i][2]));
+		}
+		return points;
+	}
+
+	let path = await pullCandleClose();
+
+  //TODO FIX MIN AND MAX VALUES
+	let minValue = Math.min(path);
+  // console.log(path);
+	// console.log(minValue);
+	// for (var i = 0; i < path.length; i++) {
+	//   path[i] = path[i] - minValue;
+	// }
+	let maxValue = Math.max(path[0]);
+	var lines = [];
+
+// Terrain
+//REVIEW WILL RENDE
+	for (let i = 0; i < path.length - 1; i++) {
+		let x = (i + (i + 1)) / 2 * 30; //*5 only used to span graph
+		let y = 750 - ((path[i] + path[i + 1]) / 2 * (350 / maxValue)); //scaling to fit area
+		// console.log(y);
+		let adjacent = 1 * 30;
+		let opposite = (path[i + 1] - path[i]) * (350 / maxValue);
+		let angle = Math.atan(opposite / adjacent);
+		// console.log(angle);
+		let line = Bodies.rectangle(x, y, Math.sqrt(opposite ** 2 + adjacent ** 2), 1, {
+			isStatic: true,
+			angle: -angle
+		});
+		// World.add(engine.world, line);
+		lines.push(line);
+	}
+
+
+	let ball = Bodies.circle(40, 20, 30);
+	// console.log(course);
+	// console.log("this");
+  World.add(engine.world, lines);
+	World.add(engine.world, ball);
+	// run the engine
+	Engine.run(engine);
+	// run the renderer
+	Render.run(render);
+};
+golf();
